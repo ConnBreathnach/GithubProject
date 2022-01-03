d7ce9262f70f8616e55cd44a748d46a56e463b3b
Conn Breathnach <cobreath@tcd.ie>
2022-01-03 15:44:48 +0000

    More template changes

--- src/app.py
+++ src/app.py
@@ -1,23 +1,24 @@
 import re
 from flask import Flask, render_template, request, redirect, url_for
-from pull_request_getter import get_pull_request
+from .pull_request_getter import get_pull_request
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import text
 from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 
+CORPUS_PATH = './skipgram/corpus.txt'
 model = load_model('skipgram/models/skipgram_model.h5')
 
 word_embedding_layer = model.get_layer(index=2)
 word_embedding_weights = word_embedding_layer.get_weights()[0][1:]
 
-def yield_strings(file_path='corpus.txt'):
+def yield_strings(file_path=CORPUS_PATH):
     with open(file_path, 'r') as f:
         for line in f:
             yield line.strip()
 
 tokenizer = text.Tokenizer()
-tokenizer.fit_on_texts(yield_strings('corpus.txt'))
+tokenizer.fit_on_texts(yield_strings(CORPUS_PATH))
 
 word2idx = tokenizer.word_index
 
@@ -29,7 +30,7 @@ def compare_prs(pr_1, pr_2, algorithm):
     pr_2_body = parse_body(pr_2_data.body)
 
     if algorithm == 'average':
-        return average_prs(pr_1_body, pr_2_data)
+        return average_prs(pr_1_body, pr_2_body)
     elif algorithm == 'wordmover':
         return wordmover(pr_1_body, pr_2_body)
     # elif algorithm == 'nn':
@@ -44,10 +45,12 @@ def parse_body(body):
     return body
 
 def average_prs(pr_1_body, pr_2_body):
-    pr_1_body = tokenizer.texts_to_sequences([pr_1_body])[0]
-    pr_2_body = tokenizer.texts_to_sequences([pr_2_body])[0]
-    pr_1_body_embedding = np.mean(word_embedding_weights[pr_1_body], axis=0)
-    pr_2_body_embedding = np.mean(word_embedding_weights[pr_2_body], axis=0)
+    pr_1_tokens = tokenizer.texts_to_sequences([pr_1_body])[0]
+    pr_2_tokens = tokenizer.texts_to_sequences([pr_2_body])[0]
+    print(pr_1_tokens)
+    pr_1_body_embedding = np.mean(word_embedding_weights[pr_1_tokens], axis=0)
+    pr_2_body_embedding = np.mean(word_embedding_weights[pr_2_tokens], axis=0)
+    print(np.linalg.norm(pr_1_body_embedding - pr_2_body_embedding))
     return np.linalg.norm(pr_1_body_embedding - pr_2_body_embedding)
 
 def wordmover(pr_1_body, pr_2_body):
@@ -58,6 +61,7 @@ def wordmover(pr_1_body, pr_2_body):
     move_cost = 0
     for word in smaller_body:
         move_cost += calculate_move_cost(word, larger_body)
+    print(move_cost)
     return move_cost
 
 
@@ -72,6 +76,10 @@ def calculate_move_cost(word_1, larger_body):
             min_cost = cost
     return min_cost
 
+def create_dataframe(pr_body):
+    distance_matrix = euclidean_distances(word_embedding_weights)
+    word_vectors = np.array([word_embedding_weights[idx] for idx in words_idxs])
+
 
 app = Flask(__name__)
 
@@ -80,14 +88,28 @@ def homepage():
     return render_template('home.html')
 
 @app.route('/', methods=['POST'])
-def compare_prs():
+def run_algorithm():
     if request.method == 'POST':
         pr_1 = request.form['pr_1']
         pr_2 = request.form['pr_2']
         algorithm = request.form['algorithm']
-        return redirect(url_for('compare_pr_route', pr_1=pr_1, pr_2=pr_2, algorithm=algorithm))
+        result = compare_prs(pr_1, pr_2, algorithm)
+        return redirect(url_for('compare_pr_result', result=result))
 
 @app.route('/compare_prs/<pr_1>/<pr_2>/<algorithm>')
-def compare_pr_route(pr_1, pr_2, algorithm):
-    result = compare_prs(pr_1, pr_2, algorithm)
+def compare_pr_result(result):
     return render_template('compare_prs.html', result=result)
+
+@app.route('embeddings')
+def embeddings_page():
+    return render_template('embeddings.html')
+
+@app.route('embeddings')
+def embeddings_post():
+    if request.method == 'POST':
+        pr = request.form['pr']
+
+
+@app.route('embeddings/data')
+def embeddings():
+    return render_template('embeddings.html')
