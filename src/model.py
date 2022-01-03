0fc0573a2c08f99146521286bc0038cff95a7419
Conn Breathnach <cobreath@tcd.ie>
2022-01-03 17:38:16 +0000

    Setup model architecture and training. Having issues with tensorflow

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import Dense, Reshape, Embedding, Dot, Input, LSTM
 from tensorflow.keras.models import Sequential, Model
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 
 def yield_strings(file_path='skipgram/corpus.txt'):
     with open(file_path, 'r') as f:
@@ -25,19 +25,30 @@ word_ids = [[word2idx[w] for w in text.text_to_word_sequence(s)] for s in yield_
 
 skipgram_model = load_model('skipgram/models/skipgram_model.h5')
 word_embedding_layer = skipgram_model.get_layer(index=2)
-word_embedding_weights = word_embedding_layer.get_weights()[0][1:]
+word_embedding_weights = word_embedding_layer.get_weights()[0]
 
-pr_1_input = Input(shape=(None,1,100))
-pr_2_input = Input(shape=(None,1,100))
-reshape_1 = Reshape((100,))(pr_1_input)
-reshape_2 = Reshape((100,))(pr_2_input)
+input_1 = Input(shape=(1,))
+input_2 = Input(shape=(1,))
 
+embedding_layer_1 = Embedding(vocab_size, embedding_dim, weights=[word_embedding_weights], input_length=1, trainable=False)(input_1)
+embedding_layer_2 = Embedding(vocab_size, embedding_dim, weights=[word_embedding_weights], input_length=1, trainable=False)(input_2)
 #Model will take 2 sentences and using a RNN will perform binary classification
-#If the sentences are similar, the output will be 1, else 0
-lstm_layer_1 = LSTM(units=100, return_sequences=False, dropout=0.2, recurrent_dropout=0.2, input_shape=(1000, 100))(pr_1_input)
-lstm_layer_2 = LSTM(units=100, return_sequences=False, dropout=0.2, recurrent_dropout=0.2, input_shape=(1000, 100))(pr_2_input)
-dense_layer = Dense(units=1, activation='sigmoid')([lstm_layer_1, lstm_layer_2])
+#If the sentences are from same repo, the output will be 1, else 0
+lstm_layer_1 = LSTM(units=100, return_sequences=False, dropout=0.2, recurrent_dropout=0.2, input_shape=(1000, 100))(embedding_layer_1)
+lstm_layer_2 = LSTM(units=100, return_sequences=False, dropout=0.2, recurrent_dropout=0.2, input_shape=(1000, 100))(embedding_layer_2)
+merge_layer = Dot(axes=1)([lstm_layer_1, lstm_layer_2])
+dense_layer = Dense(units=1, activation='sigmoid')(merge_layer)
 
-model = Model(inputs=[pr_1_input, pr_2_input], outputs=dense_layer)
+model = Model(inputs=[input_1, input_2], outputs=dense_layer)
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 print(model.summary())
+
+dataset = pd.read_csv('../data/combined_dataset.csv')
+dataset.drop(['Unnamed: 0', 'pr_title_1', 'pr_title_2', 'commits_1', 'commits_2', 'repo_1_name', 'repo_1_user', 'repo_2_name', 'repo_2_user'], axis=1, inplace=True)
+
+X = dataset.drop(['same_repo'], axis=1)
+y = dataset['same_repo']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model.fit([X_train['pr_body_1'], X_train['pr_body_2']], y_train, epochs=5, batch_size=32, validation_data=([X_test['pr_body_1'], X_test['pr_body_2']], y_test))
+model.save('same_rep_model.h5')
